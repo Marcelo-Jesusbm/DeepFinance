a526c6447eb4a633627c4bb49dbd06f1f8e7956e
Marcelo Jesus <mhsj.4001@gmail.com>
2025-07-29 11:05:47 -0700

    Initial Commit

---
+++ DeepFinance/DeepFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepFinanceApp.swift
+//  DeepFinance
+//
+//  Created by Marcelo Jesus on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DeepFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
